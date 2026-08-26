860c11f36d52eefdef0fbac33bb7480d33c19654
Dania Ahmed <166382419+daniaahmed1@users.noreply.github.com>
2026-08-26 18:09:41 +0100

    Update cli.py

--- src/airtuk/cli.py
+++ src/airtuk/cli.py
@@ -35,13 +35,13 @@ def main():
         # Create environments and install requirements
         python_executables = setup_environments()
 
-        AIRTUKT_python = python_executables["AIRTUKT"]
-        AIRTUKT2_python = python_executables["AIRTUKT2"]
+        airtukt_python = python_executables["airtukt"]
+        airtukt2_python = python_executables["airtukt2"]
 
         # Install both Jupyter kernels
         install_kernels(
-            AIRTUKT_python,
-            AIRTUKT2_python,
+            airtukt_python,
+            airtukt2_python,
         )
 
         print("\n===================================")
